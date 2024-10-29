ae2f1a4e6ce3f1377ab68cc031f54542516f75a1
Fiernes <fidel255.gu@gmail.com>
2024-10-29 12:22:17 -0600

    se subio la base de datos

---
+++ config/db_biblioteca.sql
@@ -0,0 +1,360 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-10-2024 a las 01:51:09
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carritoprestamo`
+--
+
+CREATE TABLE `carritoprestamo` (
+  `idCarrito` int(11) NOT NULL,
+  `cantidaLibros` int(11) DEFAULT NULL,
+  `idEstudiante` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito_libros`
+--
+
+CREATE TABLE `carrito_libros` (
+  `idCarrito` int(11) NOT NULL,
+  `idLibro` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direccion`
+--
+
+CREATE TABLE `direccion` (
+  `idPersona` int(11) NOT NULL,
+  `departamento` varchar(50) DEFAULT NULL,
+  `ciudad` varchar(50) DEFAULT NULL,
+  `colonia` varchar(50) DEFAULT NULL,
+  `numeroCasa` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `idEmpleado` int(11) NOT NULL,
+  `tipoEmpleado` varchar(50) DEFAULT NULL,
+  `puesto` varchar(50) DEFAULT NULL,
+  `departamento` varchar(50) DEFAULT NULL,
+  `horarioLaboral` varchar(50) DEFAULT NULL,
+  `idPersona` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado_rol`
+--
+
+CREATE TABLE `empleado_rol` (
+  `idEmpleado` int(11) NOT NULL,
+  `idRol` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiante`
+--
+
+CREATE TABLE `estudiante` (
+  `idEstudiante` varchar(12) NOT NULL,
+  `idPersona` int(11) DEFAULT NULL,
+  `carrera` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_prestamo`
+--
+
+CREATE TABLE `historial_prestamo` (
+  `idPrestamo` int(11) NOT NULL,
+  `fechaPrestamo` date DEFAULT NULL,
+  `fechaDevolucion` date DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `idEstudiante` varchar(12) DEFAULT NULL,
+  `idLibro` int(11) DEFAULT NULL,
+  `idMulta` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `infocontacto`
+--
+
+CREATE TABLE `infocontacto` (
+  `idPersona` int(11) NOT NULL,
+  `correoElectronico` varchar(50) DEFAULT NULL,
+  `telefonoCasa` varchar(50) DEFAULT NULL,
+  `telefonoPersonal` varchar(50) DEFAULT NULL,
+  `telefonoPersonalDos` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libro`
+--
+
+CREATE TABLE `libro` (
+  `idLibro` int(11) NOT NULL,
+  `titulo` varchar(50) DEFAULT NULL,
+  `autor` varchar(50) DEFAULT NULL,
+  `yearPublicacion` date DEFAULT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  `cantidadDisponible` int(11) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `logs`
+--
+
+CREATE TABLE `logs` (
+  `idLog` int(11) NOT NULL,
+  `accion` text DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `idPersona` int(11) DEFAULT NULL,
+  `detalleAccion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `multa`
+--
+
+CREATE TABLE `multa` (
+  `idMulta` int(11) NOT NULL,
+  `monto` double DEFAULT NULL,
+  `fechaMulta` date DEFAULT NULL,
+  `resumen` varchar(50) DEFAULT NULL,
+  `pagada` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `idPersona` int(11) NOT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `primerNombre` varchar(50) DEFAULT NULL,
+  `segundoNombre` varchar(50) DEFAULT NULL,
+  `primerApellido` varchar(50) DEFAULT NULL,
+  `segundoApellido` varchar(50) DEFAULT NULL,
+  `DNI` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `idRol` int(11) NOT NULL,
+  `nombreRolUno` varchar(50) DEFAULT NULL,
+  `nombreRolDos` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `carritoprestamo`
+--
+ALTER TABLE `carritoprestamo`
+  ADD PRIMARY KEY (`idCarrito`);
+
+--
+-- Indices de la tabla `carrito_libros`
+--
+ALTER TABLE `carrito_libros`
+  ADD PRIMARY KEY (`idCarrito`),
+  ADD KEY `Carrito_Libros_idLibro_fk` (`idLibro`);
+
+--
+-- Indices de la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  ADD PRIMARY KEY (`idPersona`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`idEmpleado`),
+  ADD KEY `Empleado_idPersona_fk` (`idPersona`);
+
+--
+-- Indices de la tabla `empleado_rol`
+--
+ALTER TABLE `empleado_rol`
+  ADD PRIMARY KEY (`idEmpleado`),
+  ADD KEY `Empleado_Rol_idRol_fk` (`idRol`);
+
+--
+-- Indices de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`idEstudiante`),
+  ADD KEY `Estudiante_idPersona_fk` (`idPersona`);
+
+--
+-- Indices de la tabla `historial_prestamo`
+--
+ALTER TABLE `historial_prestamo`
+  ADD PRIMARY KEY (`idPrestamo`),
+  ADD UNIQUE KEY `idMulta` (`idMulta`),
+  ADD KEY `Historial_Prestamo_idEstudiante_fk` (`idEstudiante`),
+  ADD KEY `Historial_Prestamo_idLibro_fk` (`idLibro`);
+
+--
+-- Indices de la tabla `infocontacto`
+--
+ALTER TABLE `infocontacto`
+  ADD PRIMARY KEY (`idPersona`);
+
+--
+-- Indices de la tabla `libro`
+--
+ALTER TABLE `libro`
+  ADD PRIMARY KEY (`idLibro`);
+
+--
+-- Indices de la tabla `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`idLog`),
+  ADD KEY `Logs_idPersona_fk` (`idPersona`);
+
+--
+-- Indices de la tabla `multa`
+--
+ALTER TABLE `multa`
+  ADD PRIMARY KEY (`idMulta`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`idPersona`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`idRol`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `carritoprestamo`
+--
+ALTER TABLE `carritoprestamo`
+  ADD CONSTRAINT `CarritoPrestamo_idCarrito_fk` FOREIGN KEY (`idCarrito`) REFERENCES `carrito_libros` (`idCarrito`);
+
+--
+-- Filtros para la tabla `carrito_libros`
+--
+ALTER TABLE `carrito_libros`
+  ADD CONSTRAINT `Carrito_Libros_idLibro_fk` FOREIGN KEY (`idLibro`) REFERENCES `libro` (`idLibro`);
+
+--
+-- Filtros para la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  ADD CONSTRAINT `Direccion_idPersona_fk` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`);
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `Empleado_idPersona_fk` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`);
+
+--
+-- Filtros para la tabla `empleado_rol`
+--
+ALTER TABLE `empleado_rol`
+  ADD CONSTRAINT `Empleado_Rol_idRol_fk` FOREIGN KEY (`idRol`) REFERENCES `rol` (`idRol`);
+
+--
+-- Filtros para la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `Estudiante_idPersona_fk` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`);
+
+--
+-- Filtros para la tabla `historial_prestamo`
+--
+ALTER TABLE `historial_prestamo`
+  ADD CONSTRAINT `Historial_Prestamo_idEstudiante_fk` FOREIGN KEY (`idEstudiante`) REFERENCES `estudiante` (`idEstudiante`),
+  ADD CONSTRAINT `Historial_Prestamo_idLibro_fk` FOREIGN KEY (`idLibro`) REFERENCES `libro` (`idLibro`);
+
+--
+-- Filtros para la tabla `infocontacto`
+--
+ALTER TABLE `infocontacto`
+  ADD CONSTRAINT `InfoContacto_idPersona_fk` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`);
+
+--
+-- Filtros para la tabla `logs`
+--
+ALTER TABLE `logs`
+  ADD CONSTRAINT `Logs_idPersona_fk` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`);
+
+--
+-- Filtros para la tabla `multa`
+--
+ALTER TABLE `multa`
+  ADD CONSTRAINT `multa_idMulta_fk` FOREIGN KEY (`idMulta`) REFERENCES `historial_prestamo` (`idMulta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
